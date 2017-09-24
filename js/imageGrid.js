c79d6eb9501750216c09eb228de20c533b770bd9
Kalynn <KKOSYKA@Kalynns-MacBook-Pro.local>
2017-09-24 14:26:42 -0400

    add img/drawing

--- js/imageGrid.js
+++ js/imageGrid.js
@@ -1,6 +1,6 @@
 var allImages = "";
 
-var imageNames = ['duomo','rascalCallig','cyclingMe', 'poncho', 'winterSmith','drawing', 'firenzeHorse','wallArt','smith','cycling', 'calligraphy', 'rascal'];
+var imageNames = ['duomo','rascalCallig','cyclingMe', 'poncho', 'winterSmith','drawing', 'firenzeHorse','wallArt','smith','cycling', 'calligraphy', 'rascal', 'sketchPoncho'];
 for (var i = 0; i < imageNames.length; i++) {
     allImages += '<img src="./img/' +imageNames[i]+'.jpg" height="30%" width="30%" alt="picture">';
 }
