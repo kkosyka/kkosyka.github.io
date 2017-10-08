ad5a5fe44472cbfd85675d93cd406775d1be8bc2
Kalynn <KKOSYKA@Kalynns-MacBook-Pro.local>
2017-10-07 20:51:17 -0400

    update
    add photos & typos

--- js/imageGrid.js
+++ js/imageGrid.js
@@ -1,6 +1,6 @@
 var allImages = "";
 
-var imageNames = ['duomo','rascalCallig','cyclingMe', 'poncho', 'winterSmith','drawing', 'firenzeHorse','wallArt','smith','cycling', 'calligraphy', 'rascal', 'sketchPoncho'];
+var imageNames = ['duomo','rascalCallig','cyclingMe', 'poncho', 'winterSmith','drawing', 'lakeFitz','wallArt','smith','calligraphy','cycling', 'rascal', 'firenzeHorse','sketchPoncho', 'windyRoad'];
 for (var i = 0; i < imageNames.length; i++) {
     allImages += '<img src="./img/' +imageNames[i]+'.jpg" height="30%" width="30%" alt="picture">';
 }
