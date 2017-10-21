ae9b3ea9455cf5c8d25cf43b5c6c37770f0c3b97
Kalynn <KKOSYKA@Kalynns-MacBook-Pro.local>
2017-10-21 16:40:52 -0400

    add images & update links

--- js/imageGrid.js
+++ js/imageGrid.js
@@ -1,6 +1,6 @@
 var allImages = "";
 
-var imageNames = ['duomo','rascalCallig','cyclingMe', 'poncho', 'winterSmith','drawing', 'lakeFitz','wallArt','smith','calligraphy','cycling', 'rascal', 'firenzeHorse','sketchPoncho', 'windyRoad'];
+var imageNames = ['duomo','rascalCallig','cyclingMe', 'poncho', 'drive','winterSmith','drawing', 'lakeFitz','wallArt','smith','calligraphy','wedding','cycling', 'rascal', 'firenzeHorse','horses','sketchPoncho', 'windyRoad'];
 for (var i = 0; i < imageNames.length; i++) {
     allImages += '<img src="./img/' +imageNames[i]+'.jpg" height="30%" width="30%" alt="picture">';
 }
