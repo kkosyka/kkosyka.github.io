4a4815173d3847ce5f6766d3023bb1daf9ea74d0
Kalynn <KKOSYKA@Kalynns-MacBook-Pro.local>
2017-11-02 17:12:26 -0400

    photo updates

--- js/imageGrid.js
+++ js/imageGrid.js
@@ -1,6 +1,6 @@
 var allImages = "";
 
-var imageNames = ['duomo','rascalCallig','cyclingMe', 'poncho', 'drive','winterSmith','drawing', 'lakeFitz','wallArt','smith','calligraphy','wedding','cycling', 'rascal', 'firenzeHorse','horses','sketchPoncho', 'windyRoad'];
+var imageNames = ['duomo','rascalCallig','cyclingMe', 'reflection','poncho', 'drive','winterSmith','bee','drawing', 'lakeFitz','wallArt','smith','calligraphy','wedding','cycling','reading','rascal', 'firenzeHorse','horses','sketchPoncho', 'windyRoad'];
 for (var i = 0; i < imageNames.length; i++) {
     allImages += '<img src="./img/' +imageNames[i]+'.jpg" height="30%" width="30%" alt="picture">';
 }
