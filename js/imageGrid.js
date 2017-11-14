a657b8dfc418f3fd0667e8bbabb1175fa2f8947a
Kalynn <KKOSYKA@Kalynns-MacBook-Pro.local>
2017-11-13 21:02:28 -0500

    swap w version2

--- js/imageGrid.js
+++
@@ -1,8 +0,0 @@
-var allImages = "";
-
-var imageNames = ['duomo','rascalCallig','cyclingMe', 'reflection','poncho', 'drive','winterSmith','bee','drawing', 'lakeFitz','wallArt','smith','calligraphy','wedding','cycling','reading','rascal', 'firenzeHorse','horses','sketchPoncho', 'windyRoad'];
-for (var i = 0; i < imageNames.length; i++) {
-    allImages += '<img src="./img/' +imageNames[i]+'.jpg" height="30%" width="30%" alt="picture">';
-}
-
-$('#photos').append(allImages);
